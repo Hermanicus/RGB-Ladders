fccab8a7ae4437dda3fcf09d84e483271d521a0f
Herman Lundgren <herman.lundgren@gmail.com>
2017-01-13 22:58:48 +0100

    Arduino with RGB LED and one Pot connected
     * Unknown R values soldered to RGB LED
     * This commit blinks R,G,G to verify function

---
+++ RGB-Ladder/main/main.ino
@@ -0,0 +1,36 @@
+
+
+// the setup function runs once when you press reset or power the board
+ int redPin = 9;
+  int bluePin = 10;
+  int greenPin = 11;
+  int analogPin = 0; //Reads Pot setting 0-1024
+  int val = 0;
+  
+void setup() {
+  // initialize digital pin 13 as an output.
+   pinMode(redPin, OUTPUT);
+  pinMode(greenPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
+  Serial.begin(9600);
+  }
+
+// the loop function runs over and over again forever
+void loop() {
+  val = analogRead(analogPin);
+  Serial.println(val);
+  int redNow = val / 4;
+ //Write R
+ analogWrite(redPin,redNow);
+ delay(100);
+ analogWrite(redPin,0);
+ delay(499);
+ analogWrite(greenPin,255);
+ delay(100);
+  analogWrite(greenPin,0);
+ delay(499);
+ analogWrite(bluePin,255);
+ delay(100);
+ analogWrite(bluePin,0);
+ delay(499);
+}
